ed350c662182e499f92b4e92dfa58d587fa243ed
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-10-11 21:10:18 +0530

    Initialize Hotel Service

---
+++ hotel_service/src/test/java/com/next/travel/hotel_service/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.travel.hotel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
