8400b8551fde9ae95d914ebd25a4df2bbc92cf14
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-10-11 10:57:11 +0530

    first commit

---
+++ guide_service/src/test/java/com/next/travel/guide_service/GuideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.travel.guide_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
