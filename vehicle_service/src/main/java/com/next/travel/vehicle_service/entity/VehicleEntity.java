31533167b23ede07bb3195f3ec8c77350563b698
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-10-11 15:47:44 +0530

    Add VehicleEntity

--- vehicle_service/src/main/java/com/next/travel/vehicle_service/entity/VehicleEntity.java
+++ vehicle_service/src/main/java/com/next/travel/vehicle_service/entity/VehicleEntity.java
@@ -1,7 +1,8 @@
-package com.next.travel.vehicle_service.dto;
-
+package com.next.travel.vehicle_service.entity;
 
 import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,10 +10,13 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Vehicle {
+@Entity(name = "driver")
+public class VehicleEntity {
+    @Id
     private String vehicleId;
     private String brand;
     private String category;
@@ -24,6 +28,9 @@ public class Vehicle {
     private String transmissionType;  //auto or manual
     private String availability;
     private String remark;
+
+    @Lob
+    @ElementCollection
     private List<byte[]> images;
 
     private String driverId;
