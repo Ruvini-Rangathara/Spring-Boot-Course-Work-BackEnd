6aed133225ce8ee4b5caa29aecc5cef3853a4a60
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-10-12 22:28:32 +0530

    Add exception to package service

---
+++ package_service/src/main/java/com/next/travel/package_service/exception/DuplicateException.java
@@ -0,0 +1,7 @@
+package com.next.travel.package_service.exception;
+
+public class DuplicateException extends RuntimeException{
+    public DuplicateException(String message){
+        super(message);
+    }
+}
---
+++ package_service/src/main/java/com/next/travel/package_service/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.next.travel.package_service.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+
+}
