23f96bbe0d44f9d55e4da851e5c1641cf5b77f11
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2023-10-15 20:37:50 +0530

    Update package properties

--- package_service/src/main/java/com/next/travel/package_service/controller/PackageApi.java
+++ package_service/src/main/java/com/next/travel/package_service/controller/PackageApi.java
@@ -4,6 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class PackageApi {
 
+    @Value("${guide-service-endpoint}")
+    private String guideDataEndpoint;
+
     @Value("${hotel-service-endpoint}")
     private String hotelDataEndpoint;
+
+    @Value("${vehicle-service-endpoint}")
+    private String vehicleDataEndpoint;
+
+    @Value("${user-service-endpoint}")
+    private String userDataEndpoint;
+
 }
